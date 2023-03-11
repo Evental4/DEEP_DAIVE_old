6706020ca9952e1031abb9c98c86917b8c0f3387
Evental4 <125282836+Evental4@users.noreply.github.com>
2023-03-11 10:24:45 +0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -7,7 +7,12 @@ txCreateWindow (800, 600);
 
   HDC fon= txLoadImage("fon.bmp");
   HDC dvarf= txLoadImage("dvarf_min.bmp");
+  HDC run= txLoadImage("run.bmp");
+  HDC ran= txLoadImage("ran.bmp");
   int x_dvarf =400;
+  int y_dvarf =300;
+  int x_run =0;
+  int y_run =300;
     while(!txGetAsyncKeyState (VK_ESCAPE))
 {
 
@@ -16,9 +21,9 @@ txCreateWindow (800, 600);
    txClear ();
 
     txBitBlt(txDC() ,0 ,0, 800,600, fon);
-    txTransparentBlt(txDC() , x_dvarf  ,300, 100,200,dvarf, 0 ,0 , TX_white);
-
-
+    txTransparentBlt(txDC() , x_dvarf  ,y_dvarf, 100,200,dvarf, 0 ,0 , TX_white);
+    txTransparentBlt(txDC() , x_run ,y_run, 200,300,run, 0 ,0 , TX_white);
+    txTransparentBlt(txDC() , 500  ,200, 300,400,ran, 0 ,0 , TX_white);
     if(txGetAsyncKeyState ('D'))
     {
     x_dvarf += 10;
@@ -28,7 +33,31 @@ txCreateWindow (800, 600);
     {
     x_dvarf -= 10;
     }
+    if(txGetAsyncKeyState ('W'))
+    {
+    y_dvarf -= 10;
+    }
+    if(txGetAsyncKeyState ('S'))
+    {
+    y_dvarf += 10;
+    }
+    if(txGetAsyncKeyState ('L'))
+    {
+    x_run += 10;
+    }
 
+    if(txGetAsyncKeyState ('J'))
+    {
+    x_run-= 10;
+    }
+    if(txGetAsyncKeyState ('I'))
+    {
+    y_run -= 10;
+    }
+    if(txGetAsyncKeyState ('K'))
+    {
+    y_run += 10;
+    }
    txEnd();
    txSleep(20);
 
@@ -36,9 +65,9 @@ txCreateWindow (800, 600);
 }
 
   txDeleteDC (fon);
-
+    txDeleteDC (ran );
   txDeleteDC (dvarf );
-
+    txDeleteDC (run );
 
     txTextCursor (false);
     return 0;
