35c62271797fd1750e8fd7f3d060595b0a4f9bb4
Evental4 <125282836+Evental4@users.noreply.github.com>
2023-03-13 15:35:38 +0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-
 #include "TXLib.h"
 
 int main()
@@ -7,12 +6,12 @@ txCreateWindow (800, 600);
 
   HDC fon= txLoadImage("fon.bmp");
   HDC dvarf= txLoadImage("dvarf_min.bmp");
-  HDC run= txLoadImage("run.bmp");
-  HDC ran= txLoadImage("ran.bmp");
+  HDC dark_cnaite= txLoadImage("dark_cnaite.bmp");
+  HDC ckelet= txLoadImage("ckelet.bmp");
   int x_dvarf =400;
   int y_dvarf =300;
-  int x_run =0;
-  int y_run =300;
+  int x_dark_cnaite =200;
+  int y_dark_cnaite =216;
     while(!txGetAsyncKeyState (VK_ESCAPE))
 {
 
@@ -22,8 +21,8 @@ txCreateWindow (800, 600);
 
     txBitBlt(txDC() ,0 ,0, 800,600, fon);
     txTransparentBlt(txDC() , x_dvarf  ,y_dvarf, 100,200,dvarf, 0 ,0 , TX_white);
-    txTransparentBlt(txDC() , x_run ,y_run, 200,300,run, 0 ,0 , TX_white);
-    txTransparentBlt(txDC() , 500  ,200, 300,400,ran, 0 ,0 , TX_white);
+    txTransparentBlt(txDC() , x_dark_cnaite ,y_dark_cnaite, 200,216,dark_cnaite, 0 ,0 , TX_white);
+    txTransparentBlt(txDC() ,177,176, 193,220,ckelet, 0 ,0 , TX_white);
     if(txGetAsyncKeyState ('D'))
     {
     x_dvarf += 10;
@@ -43,20 +42,20 @@ txCreateWindow (800, 600);
     }
     if(txGetAsyncKeyState ('L'))
     {
-    x_run += 10;
+    x_dark_cnaite += 10;
     }
 
     if(txGetAsyncKeyState ('J'))
     {
-    x_run-= 10;
+    x_dark_cnaite-= 10;
     }
     if(txGetAsyncKeyState ('I'))
     {
-    y_run -= 10;
+    y_dark_cnaite -= 10;
     }
     if(txGetAsyncKeyState ('K'))
     {
-    y_run += 10;
+    y_dark_cnaite += 10;
     }
    txEnd();
    txSleep(20);
@@ -64,10 +63,10 @@ txCreateWindow (800, 600);
 
 }
 
-  txDeleteDC (fon);
-    txDeleteDC (ran );
-  txDeleteDC (dvarf );
-    txDeleteDC (run );
+    txDeleteDC (fon);
+    txDeleteDC (ckelet );
+    txDeleteDC (dvarf );
+    txDeleteDC (dark_cnaite);
 
     txTextCursor (false);
     return 0;
